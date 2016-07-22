ef03f2f41aa69436689b18829dac23dbbd7c9d37
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2016-07-22 17:40:00 +0200

    remote control added

--- appq.py
+++ appq.py
@@ -4,20 +4,35 @@ import subprocess, queue, time, threading
 
 app = Flask(__name__)
 
+KEYMAP = {"left": "\x1B[D", "right": "\x1B[C", "up": "\x1B[A", "down": "\x1B[B"}
+
 films = queue.Queue()
 
 class Player(threading.Thread):
+    def send_key(self, key):
+        try:
+            self.backend.stdin.write(KEYMAP.get(key, key))
+            self.backend.stdin.flush()
+        except:
+            print(repr(key), "didn't stdin well")
     def run(self):
         while True:
             if films.empty(): time.sleep(1)
             else:
-                subprocess.call(["omxplayer",films.get()])
+                self.backend = subprocess.Popen(["omxplayer",films.get()], stdin=subprocess.PIPE, universal_newlines=True)
+                self.backend.wait()
 
-Player(daemon=True).start()
+player = Player(daemon=True)
+player.start()
 
 @app.route("/open")
 def vid_open():
     films.put(request.args.get("video_path"))
+    return redirect(url_for("home"))
+
+@app.route("/control")
+def vid_control():
+    player.send_key(request.args.get("key"))
     return redirect(url_for("home"))
 
 @app.route("/")
