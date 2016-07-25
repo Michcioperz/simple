763cfb7aa3791c30731f30f4c58b9ecad79ffdc4
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2016-07-25 22:38:14 +0200

    a little cleanup

--- appq.py
+++ appq.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
+import queue
+import subprocess
+import threading
+import time
+
 from flask import Flask, render_template, redirect, url_for, request
-import subprocess, queue, time, threading
 
 app = Flask(__name__)
 
-KEYMAP = {"left": "\x1B[D", "right": "\x1B[C", "up": "\x1B[A", "down": "\x1B[B"}
+KEYMAP = dict(left="\x1B[D", right="\x1B[C", up="\x1B[A", down="\x1B[B")
 
 films = queue.Queue()
+
 
 class Player(threading.Thread):
     def send_key(self, key):
@@ -15,29 +20,37 @@ class Player(threading.Thread):
             self.backend.stdin.flush()
         except:
             print(repr(key), "didn't stdin well")
+
     def run(self):
         while True:
-            if films.empty(): time.sleep(1)
+            if films.empty():
+                time.sleep(1)
             else:
-                self.backend = subprocess.Popen(["omxplayer",films.get()], stdin=subprocess.PIPE, universal_newlines=True)
+                self.backend = subprocess.Popen(["omxplayer", films.get()], stdin=subprocess.PIPE,
+                                                universal_newlines=True)
                 self.backend.wait()
+
 
 player = Player(daemon=True)
 player.start()
+
 
 @app.route("/open")
 def vid_open():
     films.put(request.args.get("video_path"))
     return redirect(url_for("home"))
 
+
 @app.route("/control")
 def vid_control():
     player.send_key(request.args.get("key"))
     return redirect(url_for("home"))
 
+
 @app.route("/")
 def home():
     return render_template('home.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
